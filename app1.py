403ac2e5c51f7d029b83af40fd3a6d40fb8a441c
GTG1089 <gabbymozart2@gmail.com>
2026-05-23 16:29:45 +0000

    edit mostly done

--- app1.py
+++ app1.py
@@ -1,16 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
 from tinydb import TinyDB
 app = Flask(
     __name__,
     template_folder="templates1",
     static_folder="static1"
 )
-
+app.secret_key="very_secret_document"
+db=TinyDB("db/notes.json")
 @app.route("/")
 def index():
-    return render_template("index.html")
+    zapiski = db.all()
+    msg=session.pop('wsg', None)
+    return render_template("index.html", zapiski=zapiski)
 @app.route("/dodajzapiske", methods=["POST"])
 def add():
+    
     return
 @app.route("/brisizapiske", methods=["POST"])
 #namesto del bris ker del zgleda da je function
