13f16b7b44ed8803d19f189f0467b27d3b83ae88
GTG1089 <gabbymozart2@gmail.com>
2026-05-23 16:05:37 +0000

    app1 skeleton

--- app1.py
+++ app1.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-
+from tinydb import TinyDB
 app = Flask(
     __name__,
     template_folder="templates1",
@@ -9,6 +9,15 @@ app = Flask(
 @app.route("/")
 def index():
     return render_template("index.html")
-
+@app.route("/dodajzapiske", methods=["POST"])
+def add():
+    return
+@app.route("/brisizapiske", methods=["POST"])
+#namesto del bris ker del zgleda da je function
+def bris():
+    return
+@app.route("/editzapiske", methods=["POST", "GET"])
+def edit():
+    return
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
