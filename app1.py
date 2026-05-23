fd4bfa84f819ff6aee6d49e287d5d4c795685506
GTG1089 <gabbymozart2@gmail.com>
2026-05-23 17:08:17 +0000

    delete

--- app1.py
+++ app1.py
@@ -5,23 +5,26 @@ app = Flask(
     template_folder="templates1",
     static_folder="static1"
 )
-app.secret_key="very_secret_document"
+app.secret_key="very_secret_document"#War thunder reference
 db=TinyDB("db/notes.json")
 @app.route("/")
 def index():
     zapiski = db.all()
-    return render_template("index.html", zapiski=zapiski)
+    msg = session.pop('msg', None)
+    return render_template("index.html", zapiski=zapiski, msg=msg)
 @app.route("/dodajzapiske", methods=["POST"])
 def add():
     naslov=request.form.get('naslov')
     vsebina=request.form.get("vsebina")
     if naslov and vsebina:
         db.insert({"naslov":naslov, "vsebina":vsebina})
+        session['msg'] = "Zapisek uspešno dodan!"
     return redirect(url_for("index"))
 @app.route("/brisizapiske", methods=["POST"])
-#namesto del bris ker del zgleda da je function
-def bris():
-    return
+#namesto "del" bris ker "del" zgleda da je function
+def bris_ajax(doc_id):
+    db.remove(doc_ids=[doc_id])
+    return jsonify({"success": True})
 @app.route("/editzapiske", methods=["POST", "GET"])
 def edit(doc_id):
     if request.method == "POST":
