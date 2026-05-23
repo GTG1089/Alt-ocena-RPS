0fc6837d238ac8afe3a2d515667ebfde0eb1c29a
GTG1089 <gabbymozart2@gmail.com>
2026-05-23 16:47:31 +0000

    add?

--- app1.py
+++ app1.py
@@ -10,18 +10,23 @@ db=TinyDB("db/notes.json")
 @app.route("/")
 def index():
     zapiski = db.all()
-    msg=session.pop('wsg', None)
     return render_template("index.html", zapiski=zapiski)
 @app.route("/dodajzapiske", methods=["POST"])
 def add():
-    
-    return
+    naslov=request.form.get('naslov')
+    vsebina=request.form.get("vsebina")
+    if naslov and vsebina:
+        db.insert({"naslov":naslov, "vsebina":vsebina})
+    return redirect(url_for("index"))
 @app.route("/brisizapiske", methods=["POST"])
 #namesto del bris ker del zgleda da je function
 def bris():
     return
 @app.route("/editzapiske", methods=["POST", "GET"])
-def edit():
+def edit(doc_id):
+    if request.method == "POST":
+        naslov=request.form.get("naslov")
+        vsebina=request.form.get("vsebina")
     return
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
