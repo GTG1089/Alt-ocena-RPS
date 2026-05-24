2dbd5095bb0ad5b59ccbde239bb00542991844a3
GTG1089 <gabbymozart2@gmail.com>
2026-05-24 21:52:48 +0000

    Login and register, app3

--- app3.py
+++ app3.py
@@ -27,6 +27,40 @@ init_db()
 @app.route("/")
 def index():
     return render_template("index.html")
+def register():
+    if request.method == "POST":
+        username=request.form.get("username")
+        pw=request.form.get("pw")
 
+        if username and pw:
+            conn = get_db()
+            c=conn.cursor()
+            #Dupe check for username
+            uporabnik=c.execute("SELECT * FROM users WHERE username=?", (username)).fetchone()           
+            if uporabnik:
+                session["msg"]="Uporabniško ime že obstaja!"
+                return redirect(url_for("/register"))
+            c.execute("INSERT INTO Users(username, password) Values(?,?)",(username,pw))
+            conn.commit()
+            conn.close()
+        session["msg"] = "Registracija uspešna!"
+        return redirect(url_for("login"))
+    return render_template("register.html", msg=session.pop("msg",None))
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username=request.form.get("username")
+        pw=request.form.get("pw")
+        conn=get_db()
+        c=conn.cursor()
+
+        isci_upor=c.execute("Select * from users WHERE username = ? and password= ?", (username,pw)).fetchone()
+        if isci_upor:
+            session["username"]=username
+            return redirect(url_for("index"))
+        else:
+            session["msg"]="Napačno uporabniško ime ali geslo!"
+            return redirect(url_for("login"))
+    return render_template("login.html", msg=session.pop('msg', None))
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
