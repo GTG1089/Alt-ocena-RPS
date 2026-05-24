65d3b0bbc53331d916b41528f5fa1f27e70ec375
GTG1089 <gabbymozart2@gmail.com>
2026-05-24 09:16:58 +0000

    Prerequisites for app2

--- app2.py
+++ app2.py
@@ -1,14 +1,39 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+from tinydb import TinyDB
 app = Flask(
     __name__,
-    template_folder="templates2",
-    static_folder="static2"
+    template_folder="templates1",
+    static_folder="static1"
 )
-
+app.secret_key="very_secret_document"#War thunder reference
+db=TinyDB("db/notes.json")
 @app.route("/")
 def index():
-    return render_template("index.html")
-
+    notes = db.all()
+    msg = session.pop('msg', None)
+    return render_template("index.html", notes=notes, msg=msg)
+@app.route("/dodajzapiske", methods=["POST"])
+def add():
+    naslov=request.form.get("naslov")
+    vsebina=request.form.get("vsebina")
+    if naslov and vsebina:
+        db.insert({"naslov":naslov, "vsebina":vsebina})
+        session['msg'] = "Zapisek uspešno dodan!"
+    return redirect(url_for("index"))
+@app.route("/brisizapiske/<int:doc_id>", methods=["POST"])
+#namesto "del" bris ker "del" zgleda da je function
+def bris_ajax(doc_id):
+    db.remove(doc_ids=[doc_id])
+    return jsonify({"success": True})
+@app.route("/editzapiske/<int:doc_id>", methods=["POST", "GET"])
+def edit(doc_id):
+    if request.method == "POST":
+        naslov=request.form.get("naslov")
+        vsebina=request.form.get("vsebina")
+        db.update({"naslov":naslov, "vsebina":vsebina}, doc_ids=[doc_id])
+        session["msg"]="Zapisek uspešno updatan!"
+        return redirect(url_for("index"))
+    note=db.get(doc_id=doc_id)
+    return render_template("edit.html", note=note, doc_id=doc_id)
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
