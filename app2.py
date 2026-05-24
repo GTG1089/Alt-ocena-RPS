5c031ad38f5e4443032606730c81243f25b9a40a
GTG1089 <gabbymozart2@gmail.com>
2026-05-24 15:30:15 +0000

    app2 register

--- app2.py
+++ app2.py
@@ -16,7 +16,22 @@ def index():
     objave.reverse()
     msg = session.pop('msg', None)
     return render_template("index.html", objave=objave, msg=msg)
-@app.route("/dodajzapiske", methods=["POST"])
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username=request.form.get("username")
+        pw=request.form.get("pw")
+
+        if username and pw:
+            if upor_seznam.search(user.username==username):
+                session['msg']="Uporabniško ime že obstaja!"
+                return redirect(url_for("register"))
+        
+        upor_seznam.insert({"username": username, "password": pw})
+        session['msg'] = "Registracija uspešna!"
+    msg = session.pop('msg', None)
+    return render_template("register.html", msg=msg)
+@app.route("/dodajobjavo", methods=["POST"])
 def add():
     naslov=request.form.get("naslov")
     vsebina=request.form.get("vsebina")
@@ -24,8 +39,7 @@ def add():
         db.insert({"naslov":naslov, "vsebina":vsebina})
         session['msg'] = "Zapisek uspešno dodan!"
     return redirect(url_for("index"))
-@app.route("/brisizapiske/<int:doc_id>", methods=["POST"])
-#namesto "del" bris ker "del" zgleda da je function
+@app.route("/brisiobjavo/<int:doc_id>", methods=["POST"])
 def bris_ajax(doc_id):
     db.remove(doc_ids=[doc_id])
     return jsonify({"success": True})
